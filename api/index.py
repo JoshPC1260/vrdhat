d82856833ee044bcbc33977100cc4ca6e32bf613
JoshPC1260 <104276459+JoshPC1260@users.noreply.github.com>
2024-04-15 17:17:49 -0500

    coma

--- api/index.py
+++ api/index.py
@@ -150,4 +150,4 @@ def test():
     return jsonify({"message" : "test"})
 
 if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0" port = 8080)
+    app.run(debug=True, host="0.0.0.0", port = 8080)
