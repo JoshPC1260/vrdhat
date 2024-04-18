d08ebaecd88a570ce1020e80408f91c3bd794669
JoshPC1260 <104276459+JoshPC1260@users.noreply.github.com>
2024-04-18 13:10:47 -0500

    correcion errores

--- src/app/complete_report/page.js
+++ src/app/complete_report/page.js
@@ -1,4 +1,3 @@
-g
 "use client"
 import React, { useState, useEffect } from 'react';
 import Image from "next/image";
--- src/app/user_report/page.js
+++ src/app/user_report/page.js
@@ -1,4 +1,3 @@
-
 "use client";
 import React, { useState, useEffect } from 'react';
 import { Player, Controls } from '@lottiefiles/react-lottie-player';
